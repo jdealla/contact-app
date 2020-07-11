50b34f6782e431a27eccc619ca0a9f76c028469d
John D'Alessandro <jdealla@gmail.com>
2020-07-11 18:09:26 -0400

    refactored people

---
+++ src/components/content/people/header_row/header_row.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import {
+    TabletLandscapeAndLarger,
+    TabletPortraitAndLarger,
+    Default,
+  } from '../../../breakpoints';
+  import Visibility from '../../../../img/visibility.svg';
+
+
+const HEADER_ROW_DATA = [
+    {
+      id: 'checkbox',
+      data: null,
+      type: 'string',
+    },
+    {
+      id: 'visibility',
+      Data: Visibility,
+      type: 'image',
+    },
+    {
+      id: 'avatar',
+      Data: null,
+      type: 'string',
+    },
+    {
+      id: 'name',
+      Data: 'Name',
+      type: 'string',
+    },
+    {
+      id: 'tags',
+      Data: 'Tags',
+      type: 'string',
+    },
+    {
+      id: 'connections',
+      Data: 'Connections',
+      type: 'string',
+    },
+  ];
+
+const HeaderRow = () => (
+    <div className="header-row">
+        {HEADER_ROW_DATA.map(({ id, Data, type }) => {
+        let Breakpoint = Default;
+        if (id === 'connections') {
+            Breakpoint = TabletLandscapeAndLarger;
+        }
+        if (id === 'tags') {
+            Breakpoint = TabletPortraitAndLarger;
+        }
+        if (type === 'string') {
+            return (
+            <Breakpoint key={id}>
+                <div className={id}>{Data}</div>
+            </Breakpoint>
+            );
+        } else {
+            return (
+            <Breakpoint key={id}>
+                <Data className={id} />
+            </Breakpoint>
+            );
+        }
+        })}
+  </div>
+);
+
+export default hot(module)(HeaderRow);
--- src/components/content/people/people.js
+++ src/components/content/people/people.js
@@ -1,156 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { hot } from 'react-hot-loader';
 import './people.css';
-import Visibility from '../../../img/visibility.svg';
 import Contact from './contact/contact';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import {
-  TabletLandscapeAndLarger,
-  TabletPortraitAndLarger,
-  Default,
-} from '../../../components/breakpoints';
+import HeaderRow from './header_row/header_row';
 import Skeleton from 'react-loading-skeleton';
 
-const PRIMARY_API_URL =
-  'https://s3.amazonaws.com/exercise.realmagic.ai/mock-contact-data/contacts_0.json';
+const PRIMARY_API_URL = 'https://s3.amazonaws.com/exercise.realmagic.ai/mock-contact-data/contacts_0.json';
 
-const HEADER_ROW_DATA = [
-  {
-    id: 'checkbox',
-    data: null,
-    type: 'string',
-  },
-  {
-    id: 'visibility',
-    Data: Visibility,
-    type: 'image',
-  },
-  {
-    id: 'avatar',
-    Data: null,
-    type: 'string',
-  },
-  {
-    id: 'name',
-    Data: 'Name',
-    type: 'string',
-  },
-  {
-    id: 'tags',
-    Data: 'Tags',
-    type: 'string',
-  },
-  {
-    id: 'connections',
-    Data: 'Connections',
-    type: 'string',
-  },
-];
+const loader = (<div className="lds-container"><div className="lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>);
 
-class People extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contacts: [],
-      nextLink: null,
-    };
-  }
+const People = () => {
+  const [contacts, setContacts] = useState([]);
+  const [nextLink, setNextLink] = useState(PRIMARY_API_URL);
 
-  getContactData() {
-    const { nextLink } = this.state;
-    if (typeof nextLink === 'undefined') {
+  const getContactData = async (url) => {
+    let data = await fetch(url);
+    data = await data.json();
+    return data;
+  };
+
+  const refreshContactData = async () => {
+    if (typeof nextLink === 'undefined' || !nextLink) {
       return;
     }
-    const URL = nextLink ? nextLink : PRIMARY_API_URL;
-    fetch(URL)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          const { value, nextLink } = result;
-          this.setState((state) => {
-            const contacts = [...state.contacts, ...value];
-            return {
-              contacts,
-              nextLink,
-            };
-          });
-        },
-        (error) => {
-          this.setState({
-            error,
-          });
-        }
-      );
-  }
+    const data = await getContactData(nextLink);
+    setContacts([...contacts, ...data.value]);
+    setNextLink(data.nextLink);
+  };
 
-  componentDidMount() {
-    this.getContactData(PRIMARY_API_URL);
-  }
+  useEffect(() => {
+    refreshContactData();
+  }, []);
 
-  render() {
-    return (
-      <div className="people">
-        <div className="header-row">
-          {HEADER_ROW_DATA.map(({ id, Data, type }) => {
-            let Breakpoint = Default;
-            if (id === 'connections') {
-              Breakpoint = TabletLandscapeAndLarger;
-            }
-            if (id === 'tags') {
-              Breakpoint = TabletPortraitAndLarger;
-            }
-            if (type === 'string') {
-              return (
-                <Breakpoint key={id}>
-                  <div className={id}>{Data}</div>
-                </Breakpoint>
-              );
-            } else {
-              return (
-                <Breakpoint key={id}>
-                  <Data className={id} />
-                </Breakpoint>
-              );
-            }
-          })}
-        </div>
-        <div className="contact-list-container">
-          {this.state.contacts.length > 0 ? (
-            <InfiniteScroll
-              dataLength={this.state.contacts.length}
-              next={this.getContactData.bind(this)}
-              refreshFunction={() => this.getContactData.bind(this)}
-              hasMore={this.state.nextLink}
-              loader={
-                <div className="lds-container">
-                  <div className="lds-default">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                  </div>
-                </div>
-              }
-              scrollThreshold={'10px'}>
-              {this.state.contacts.map((contact) => {
-                return <Contact {...contact} key={contact.id} />;
-              })}
-            </InfiniteScroll>
-          ) : (
-            <Skeleton className={'skeleton-loader'} count={15} />
-          )}
-        </div>
+  return (
+    <div className="people">
+      <HeaderRow />
+      <div className="contact-list-container">
+        {contacts.length > 0 ? (
+          <InfiniteScroll
+            dataLength={contacts.length}
+            next={refreshContactData}
+            refreshFunction={refreshContactData}
+            hasMore={nextLink}
+            loader={loader}
+            scrollThreshold={'10px'}>
+            {contacts.map((contact) => {
+              return <Contact {...contact} key={contact.id} />;
+            })}
+          </InfiniteScroll>
+        ) : (
+          <Skeleton className={'skeleton-loader'} count={15} />
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default hot(module)(People);
